27c182ad8a66942f334b53836cb4a334f682fb14
durjoyrozario <88568472+durjoyrozario@users.noreply.github.com>
2026-01-24 17:25:04 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,118 @@
+const channels = [              
+              { 
+                name: "Star Jalsha", 
+                sources: [
+                    { name: "Server 1", url: "https://tvsen1.aynaott.com/gUX8BJmNc2yF/index.m3u8" },
+                    { name: "Server 2", url: "https://live.mncdn.shop/f07177e9-d1a9-48b4-8e2e-1718d098bbfb/index.m3u8" },
+                    { name: "Server 3", url: "https://satoshi-cors.herokuapp.com/http://tvn2.chowdhury-shaheb.com/starjalsha/index.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/WpQ9PsZ3/star-jalsha.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+            { 
+                name: "Zee Bangla", 
+                sources: [
+                    { 
+                        name: "SD", 
+                        url: "https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeebangla/default/manifest.mpd",
+                        type: "dash",
+                        drm: {
+                            kid: "1df83e058b864da896c9288eef63b113",
+                            key: "fc32f26239054b56aab48225305598fe"
+                        }
+                    }
+                ], 
+                img: "https://i.postimg.cc/wj9Q6Mj6/zee-bangla.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+            { 
+                name: "Colors Bangla", 
+                sources: [
+                    { name: "Server 1", url: "https://tvsen3.aynaott.com/u3LkNQ7UHhFX/index.m3u8" },
+                ], 
+                img: "https://i.postimg.cc/vTQZ4Jg6/colors-bangla.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+            { 
+                name: "Sun Bangla", 
+                sources: [
+                    { name: "Server 1", url: "https://smart.bengaldigital.live/sun-bangla-paid/tracks-v1a1/mono.m3u8" },
+                ], 
+                img: "https://i.postimg.cc/Yqvwd2HC/sun-bangla.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+            { 
+                name: "Enterr10 Bangla", 
+                sources: [
+                    { name: "Server 1", url: "https://live-bangla.akamaized.net/liveabr/playlist.m3u8" },
+                ], 
+                img: "https://i.postimg.cc/50rsyVks/enter10-bangla.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+             { 
+                name: "Sony Aath", 
+                sources: [
+                    { name: "Server 1", url: "https://livecdn-bostaflix.global.ssl.fastly.net/live.php?id=707e4efbda57&e=.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/y8WtVLd6/sony-aath.jpg", 
+                category: "Entertainment",
+                description: "India"
+            },
+            { 
+                name: "Somoy TV", 
+                sources: [
+                    { name: "Server 1", url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1702/output/index.m3u8" },
+                    { name: "Server 2", url: "https://tplay.live/out/news/somoy-tv/index.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/HsVVr6FH/somoy-tv.png", 
+                category: "News",
+                description: "Bangladesh"
+            },
+            { 
+                name: "Jamuna Television", 
+                sources: [
+                    { name: "Server 1", url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1701/output/index.m3u8" },
+                    { name: "Server 2", url: "https://tplay.live/out/news/jamuna-tv/index.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/dtR7Gh4t/jamuna-tv.png", 
+                category: "News",
+                description: "Bangladesh"
+            },
+            { 
+                name: "Independent TV",
+                sources: [
+                    { name: "Server 1", url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1704/output/index.m3u8" },
+                    { name: "Server 2", url: "https://tplay.live/out/news/independent-tv/index.m3u8" },
+                    { name: "Server 3", url: "https://bongoflixbd.top/stream.php?id=22f28ae5-8c15-4904-b9f6-70bbd94cbc43&e.m3u8" },
+
+                ], 
+                img: "https://i.postimg.cc/3rL7TH81/Independent-TV.png", 
+                category: "News",
+                description: "Bangladesh"
+            },
+            { 
+                name: "Channel 24",
+                sources: [
+                    { name: "Server 1", url: "https://ch24cdn.ncare.live/channel24/ch24office/index.m3u8" },
+                    { name: "Server 2", url: "https://tplay.live/out/news/channel-24/index.m3u8" },
+                    { name: "Server 3", url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1703/output/index.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/QdqkCSFX/channel-24.png", 
+                category: "News",
+                description: "Bangladesh"
+            },
+            { 
+                name: "News 24",
+                sources: [
+                    { name: "Server 1", url: "https://tplay.live/out/news/news-24/index.m3u8" }
+                ], 
+                img: "https://i.postimg.cc/bNJny8Qw/news-24.png", 
+                category: "News",
+                description: "Bangladesh"
+            },
+         ];
